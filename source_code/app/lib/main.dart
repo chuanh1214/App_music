66eb8e00e4e83013f3743d5bfeedf189584a95d6
Anh Chu <chuthia8@gmail.com>
2023-05-24 09:57:49 +0700

    up source code app music

---
+++ source_code/app/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:app/login/request.dart';
+import 'package:app/sources/landing.dart';
+import 'package:flutter/material.dart';
+
+import 'login/login.dart';
+
+void main() {
+  runApp(const AppMusic());
+}
+
+class AppMusic extends StatefulWidget {
+  const AppMusic({super.key});
+
+  @override
+  State<AppMusic> createState() => _AppMusicState();
+}
+
+class _AppMusicState extends State<AppMusic> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Page(),
+    );
+  }
+}
+
+class Page extends StatelessWidget {
+  const Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Landing());
+  }
+}
